4ffe594fdbd4ae18160a4aaea4a8845291ebfcab
Pablo Damaso <pablo_sebastian_damaso@gap.com>
2016-07-06 19:47:09 -0400

    prime factors kata

---
+++ src/test/java/kata/primefactors/PrimeFactorsTest.java
@@ -0,0 +1,52 @@
+package kata.primefactors;
+
+import junit.framework.TestCase;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static kata.primefactors.PrimeFactors.generate;
+
+public class PrimeFactorsTest extends TestCase {
+
+    private List<Integer> list(int... args) {
+        List<Integer> list = new ArrayList<Integer>();
+        for (int i : args) {
+            list.add(i);
+        }
+        return list;
+    }
+
+    public void testOne() throws Exception {
+        new PrimeFactors();
+        assertEquals(list(), generate(1));
+    }
+
+    public void testTwo() throws Exception {
+        assertEquals(list(2), generate(2));
+    }
+
+    public void testThree() throws Exception {
+        assertEquals(list(3), generate(3));
+    }
+
+    public void testFour() throws Exception {
+        assertEquals(list(2,2), generate(4));
+    }
+
+    public void testSix() throws Exception {
+        assertEquals(list(2,3), generate(6));
+    }
+
+    public void testEight() throws Exception {
+        assertEquals(list(2,2,2), generate(8));
+    }
+
+    public void testNine() throws Exception {
+        assertEquals(list(3,3), generate(9));
+    }
+
+    public void testElse() throws Exception {
+        assertEquals(list(2,2,2,2,3,3), generate(144));
+    }
+}
